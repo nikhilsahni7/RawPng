c46705bc2a9cefed510ef19feff2d7ba18f9c868
Nikhil Sahni <nikhil.sahni321@gmail.com>
2024-12-27 13:09:23 +0530

    "added api/auth/session route file"

---
+++ src/app/api/auth/session/route.ts
@@ -0,0 +1,35 @@
+import { NextResponse } from "next/server";
+import { cookies } from "next/headers";
+import { verify } from "jsonwebtoken";
+import { User } from "@/lib/models/user";
+import { connectDB } from "@/lib/db";
+
+export async function GET() {
+  try {
+    const token = cookies().get("auth_token")?.value;
+
+    if (!token) {
+      return NextResponse.json({ user: null });
+    }
+
+    const decoded = verify(token, process.env.JWT_SECRET!) as {
+      userId: string;
+    };
+    await connectDB();
+
+    const user = await User.findById(decoded.userId).select("email");
+    if (!user) {
+      return NextResponse.json({ user: null });
+    }
+
+    return NextResponse.json({
+      user: {
+        id: user._id,
+        email: user.email,
+      },
+    });
+  } catch (error) {
+    console.error(error);
+    return NextResponse.json({ user: null });
+  }
+}
